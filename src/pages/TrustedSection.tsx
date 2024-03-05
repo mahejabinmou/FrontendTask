6d08a9dafcab066e0ba71ddefb9475717eb6873e
Mou <mahejabinmou.99@gmail.com>
2024-03-04 22:17:35 -0800

    Trusted Section done

---
+++ src/pages/TrustedSection.tsx
@@ -0,0 +1,19 @@
+const TrustedSection = () => {
+  return (
+    <div className="h-full max-w-[1140px] w-full px-[120px] mt-20 ">
+      <h1 className="text-center  mb-8">Trusted by 45M+ users</h1>
+      <div
+        className="flex
+           justify-between items-center gap-x-[104px]"
+      >
+        <img src="src/assets/images/Walmart_Index_Only.svg.png" alt="" />
+        <img src="src/assets/images/cisco.svg.png" alt="" />
+        <img src="src/assets/images/Volvo_Index_Only.svg.png" alt="" />
+        <img src="src/assets/images/deloitte.svg.png" alt="" />
+        <img src="src/assets/images/okta.svg.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default TrustedSection;
